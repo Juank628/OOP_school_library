b81cb7ae39103b18da6fedbed8654a52f1f228d6
Juank628 <jsanchez_zunino@yahoo.com>
2023-03-09 16:04:40 -0500

    split main in many functions

--- app.rb
+++ app.rb
@@ -2,6 +2,43 @@ require_relative './classes/book'
 require_relative './classes/student'
 require_relative './classes/teacher'
 
+def option1
+  Book.all.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+end
+
+def option2
+  Student.all.each { |student| puts "[Student] Name: #{student.name}, ID: #{student.id}, Age: #{student.age}" }
+  Teacher.all.each { |teacher| puts "[Teacher] Name: #{teacher.name}, ID: #{teacher.id}, Age: #{teacher.age}" }
+end
+
+def option3
+  puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]'
+  type = gets.chomp.to_i
+  option3_type1 if type == 1
+  option3_type2 if type == 2
+end
+
+def option3_type1
+  print 'Age: '
+  age = gets
+  print 'Name: '
+  name = gets.chomp
+  print 'Has parent permission? [Y/N]: '
+  permission = gets.downcase
+  permission_traslator = { y: true, n: false }
+  Student.new(age, nil, name, parent_permission: permission_traslator[permission])
+end
+
+def option3_type2
+  print 'Age: '
+  age = gets
+  print 'Name: '
+  name = gets.chomp
+  print 'Specialization: '
+  specialization = gets.chomp
+  Teacher.new(age, specialization, name)
+end
+
 def main
   interface_active = true
 
@@ -17,39 +54,9 @@ def main
     puts '7 - Exit'
     option = gets.chomp.to_i
 
-    if option == 1
-      Book.all.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
-    end
-
-    if option == 2
-      Student.all.each { |student| puts "[Student] Name: #{student.name}, ID: #{student.id}, Age: #{student.age}" }
-      Teacher.all.each { |teacher| puts "[Teacher] Name: #{teacher.name}, ID: #{teacher.id}, Age: #{teacher.age}" }
-    end
-
-    if option == 3
-      puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]'
-      permission_traslator = { y: true, n: false }
-      type = gets.chomp.to_i
-      if type == 1
-        print 'Age: '
-        age = gets
-        print 'Name: '
-        name = gets.chomp
-        print 'Has parent permission? [Y/N]: '
-        permission = gets.downcase
-        Student.new(age, nil, name, parent_permission: permission_traslator[permission])
-      end
-
-      if type == 2
-        print 'Age: '
-        age = gets
-        print 'Name: '
-        name = gets.chomp
-        print 'Specialization: '
-        specialization = gets.chomp
-        Teacher.new(age, specialization, name)
-      end
-    end
+    option1 if option == 1
+    option2 if option == 2
+    option3 if option == 3
 
     interface_active = false if option == 7
 
