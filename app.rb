6fa2ff488e9cffc98de745a70b723ca0a5251d65
Juank628 <jsanchez_zunino@yahoo.com>
2023-03-09 13:37:15 -0500

    User menu options 1 and 2 implemented

--- app.rb
+++ app.rb
@@ -1,25 +1,34 @@
 require_relative './classes/book'
+require_relative './classes/student'
 
 def main
   interface_active = true
 
   while interface_active
-    p 'Welcome to School Library App!'
-    p 'Please choose an option by enterin a number:'
-    p '1 - List of all books'
-    p '2 - List of all people'
-    p '3 - Create a person'
-    p '4 - Create a book'
-    p '5 - Create a rental'
-    p '6 - List all rental for a given person id'
-    p '7 - Exit'
-    option = gets.chomp
+    puts 'Welcome to School Library App!'
+    puts 'Please choose an option by enterin a number:'
+    puts '1 - List of all books'
+    puts '2 - List of all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rental for a given person id'
+    puts '7 - Exit'
+    option = gets.chomp.to_i
 
-    if option = 1
-        
+    if option == 1
+      Book.new('a', 'b')
+      Book.all.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
     end
 
-    interface_active = false if option == '7'
+    if option == 2
+      Student.new(42, "A1")
+      Student.all.each { |student| puts "[Student] Name: #{student.name}, ID: #{student.id}, Age: #{student.age}" }
+    end
+
+
+
+    interface_active = false if option == 7
 
   end
 end
